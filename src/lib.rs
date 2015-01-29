9b160945ada69ebcf79c7ca96a3a510c924921ff
Coeuvre Wong <coeuvre@gmail.com>
2015-01-29 18:38:15 +0800

    Remove unnecessray `use`

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,5 @@
 extern crate image;
 
-//pub use packer::Packer;
-//pub use shelf_packer::ShelfPacker;
-//pub use guillotine_packer::GuillotinePacker;
-//pub use maxrect_packer::MaxrectPacker;
-//pub use skyline_packer::SkylinePacker;
-//pub use buffer2d::Buffer2d;
-//pub use image_buffer::ImgBuffer;
-//pub use color::{
-    //ColorType,
-    //Color,
-//};
-
-//mod packer;
-//mod shelf_packer;
-//mod guillotine_packer;
-//mod maxrect_packer;
-//mod skyline_packer;
-//mod buffer2d;
-//mod image_buffer;
-//mod color;
-
 pub use rect::Rect;
 pub use frame::Frame;
 pub use texture_packer::TexturePacker;
