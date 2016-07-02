39473589fe84be306c8c7e4241621be4706d793b
Hyeon Kim <simnalamburt@gmail.com>
2016-07-03 01:02:04 +0900

    Simplify the sample code

--- examples/packer-test.rs
+++ examples/packer-test.rs
@@ -5,37 +5,49 @@ use std::path::Path;
 use std::fs::File;
 
 use texture_packer::texture::Texture;
-use texture_packer::{ TexturePacker, TexturePackerConfig };
+use texture_packer::{TexturePacker, TexturePackerConfig};
 use texture_packer::importer::ImageImporter;
 use texture_packer::exporter::ImageExporter;
 
-const MAX_IMAGE_WIDTH: u32 = 400;
-const MAX_IMAGE_HEIGHT: u32 = 400;
-
 fn main() {
-    let mut config = TexturePackerConfig::default();
-    config.max_width = MAX_IMAGE_WIDTH;
-    config.max_height = MAX_IMAGE_HEIGHT;
-    config.allow_rotation = false;
-    config.texture_outlines = true;
-    config.border_padding = 2;
+    //
+    // Perform texture packing
+    //
+    let config = TexturePackerConfig {
+        max_width: 400,
+        max_height: 400,
+        allow_rotation: false,
+        texture_outlines: true,
+        border_padding: 2,
+        ..Default::default()
+    };
 
-    let ref mut texture_packer = TexturePacker::new_skyline(config);
+    let mut packer = TexturePacker::new_skyline(config);
 
-    for i in 1 .. 11 {
-        let file = format!("{}.png", i);
-        let ref path = ["./examples/assets/", &file[..]].concat();
-        let ref path = Path::new(path);
-        let texture = ImageImporter::import_from_file(path).unwrap();
+    for i in 1..11 {
+        let name = format!("{}.png", i);
+        let path = format!("examples/assets/{}", name);
+        let path = Path::new(&path);
+        let texture = ImageImporter::import_from_file(&path).unwrap();
 
-        texture_packer.pack_own(file, texture);
+        packer.pack_own(name, texture);
     }
 
-    let image = ImageExporter::export(texture_packer).unwrap();
-    let path = "./examples/output/skyline-packer-output.png";
-    let ref path = Path::new(path);
-    let ref mut file = File::create(path).unwrap();
 
-    println!("{} x {}", texture_packer.width(), texture_packer.height());
-    image.save(file, image::PNG).unwrap();
+    //
+    // Print the information
+    //
+    println!("Dimensions : {}x{}", packer.width(), packer.height());
+    println!("");
+    for (name, frame) in packer.get_frames() {
+        println!("  {:7} : {:?}", name, frame.frame);
+    }
+
+
+    //
+    // Save the result
+    //
+    let exporter = ImageExporter::export(&packer).unwrap();
+    let mut file = File::create("examples/output/skyline-packer-output.png").unwrap();
+    exporter.save(&mut file, image::PNG).unwrap();
 }
