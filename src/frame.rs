268117ff05c2ee943f115644181cc627b562523a
Kevin Robert Stravers <macocio@gmail.com>
2020-03-01 12:38:27 +0100

    docs: Document the frame module contents

--- src/frame.rs
+++ src/frame.rs
@@ -1,10 +1,15 @@
 use crate::rect::Rect;
 
+/// Boundaries and properties of a packed texture.
 #[derive(Clone, Debug)]
 pub struct Frame {
+    /// Key used to uniquely identify this frame.
     pub key: String,
+    /// Rectangle describing the texture coordinates and size.
     pub frame: Rect,
+    /// True if the texture was rotated during packing.
     pub rotated: bool,
+    /// True if the texture was trimmed during packing.
     pub trimmed: bool,
 
     // (x, y) is the trimmed frame position at original image
@@ -21,5 +26,6 @@ pub struct Frame {
     //     |  *********   |
     //     |              |
     //     +--------------+
+    /// Source texture size before any trimming.
     pub source: Rect,
 }
